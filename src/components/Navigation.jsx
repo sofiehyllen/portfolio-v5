bfc5c51e79cffed905ef9484687e7a31359c8de5
sofiehyllen <sofiehyllen@hotmail.com>
2026-03-12 11:33:58 +0100

    made entire application responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,14 +5,23 @@ export default function Navigation() {
 	const { t } = useTranslation("navigation");
 
 	return (
-		<nav className="pb-0.5 flex space-x-10">
-			<Link to="/projects" className="font-display font-bold">
+		<nav className="pb-0.5 flex space-x-5 md:space-x-10">
+			<Link
+				to="/projects"
+				className="font-display font-semibold text-sm md:text-base md:font-bold"
+			>
 				{t("projects")}
 			</Link>
-			<Link to="/about" className="font-display font-bold">
+			<Link
+				to="/about"
+				className="font-display font-semibold text-sm md:text-base md:font-bold"
+			>
 				{t("about")}
 			</Link>
-			<Link to="/contact" className="font-display font-bold">
+			<Link
+				to="/contact"
+				className="font-display font-semibold text-sm md:text-base md:font-bold"
+			>
 				{t("contact")}
 			</Link>
 		</nav>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -11,6 +11,7 @@ export default function ProjectsPage() {
 			subtitle={t("projects.subtitle")}
 		>
 			<ProjectCard />
+			<ProjectCard />
 		</PageLayout>
 	);
 }
